22105fe727ebfec3ba746722df978d24c3083e71
vampire-wpre <3222369036@qq.com>
2023-10-17 21:08:57 +0800

    2023_10_17

---
+++ 2023_10_17/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int num = 1234, s = 0;
+//	while (num != 0)
+//	{
+//		s += num % 10;
+//		num /= 10;
+//	}
+//	printf("%d", s);//输出10
+//	return 0;
+//}
+//int main()
+//{
+//	char ch;
+//	int n = 0;
+//	for (ch = getchar(); ch != '\n'; n++);//死循环，for循环的初始化语句只会执行一次
+//	return 0;
+//}
+//int main()
+//{
+//	char s[8]; 
+//	//s = "Beijing";数组名是常量值，不可以修改
+//	return 0;
+//}
