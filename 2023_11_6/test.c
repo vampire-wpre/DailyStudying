461072b0333a25ac65d2e31106df930875def547
vampire-wpre <3222369036@qq.com>
2023-11-06 22:34:17 +0800

    2023_11_6/test.c

---
+++ 2023_11_6/test.c
@@ -0,0 +1,110 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+
+//int main()
+//{
+//
+//	int x1, y1;
+//	int x2,y2;
+//	scanf("%d %d", &x1, &y1);
+//	scanf("%d %d", &x2, &y2);
+//	int ret = 0;
+//	ret = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+//«Û«∞n
+//#include <stdio.h>
+//
+//int main()
+//{
+//    long long a;
+//    scanf("%lld", &a);
+//    long long sum = 0;
+//    for (long long i = 0; i <= a; i++)
+//    {
+//        sum += i;
+//    }
+//    printf("%lld\n", sum);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	int x = 0;
+//	int count = 0;
+//	scanf("%d %d", &n,&x);
+//	for (int i = 1; i <= n; i++)
+//	{
+//		int tmp = i;
+//		//for (int j = 0; j < ret; j++)
+//		//{
+//		//	//if (tmp % 10 == x)
+//		//	//{
+//		//	//	count++;
+//		//	//}
+//		//	//tmp /= 10;
+//		//}
+//		while (tmp)
+//		{
+//			if (tmp % 10 == x)
+//			{
+//				count++;
+//			}
+//			tmp /= 10;
+//		}
+//
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    int N = 0;
+//    int ret = 0;
+//    int sum = 0;
+//    scanf("%d", &N);
+//    for (int i = 0; i < N; i++)
+//    {
+//        scanf("%d", &ret);
+//        sum += ret;
+//    }
+//    printf("%d\n", sum);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int n = 0;
+//    int ret = 0;
+//    int max = 0;
+//    int min = 100;
+//    scanf("%d", &n);
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d", &ret);
+//        if (ret > max)
+//        {
+//            max = ret;
+//        }
+//        if (ret < min)
+//        {
+//            min = ret;
+//        }
+//    }
+//
+//
+//    printf("%d\n", max - min);
+//    return 0;
+//}
+//10
+//98 100 99 97 95 99 98 97 96 100
